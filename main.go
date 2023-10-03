8cca7f3f3e8baa79b7d1675623d858a031063e7f
Satyaansh <satyanshasthana@gmail.com>
2023-10-03 16:21:22 +0530

    Add flags

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"flag"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	
+	//decalring subcommand
+	defCmd := flag.NewFlagSet("", flag.ExitOnError)
+
+
+	//inputs for defCmd
+	byteCount := defCmd.Bool("c", false, "Get number of bytes in file")
+	lineCount := defCmd.Bool("l", false, "Get number of lines in file")
+	wordCount := defCmd.Bool("w", false, "Get number of words in file")
+	charCount := defCmd.Bool("m", false, "Get number of characters in file")
+	byteLineWordCount := defCmd.Bool("", false, "Get number of bytes, lines, words in file")
 }
