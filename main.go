f545c82cee0c955dd39df92417a36db58476d264
Satyaansh <satyanshasthana@gmail.com>
2023-10-04 18:25:53 +0530

    Remove flagset and add Args check

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
 	
-	//decalring subcommand
-	defCmd := flag.NewFlagSet("", flag.ExitOnError)
-
-
 	//inputs for defCmd
-	byteCount := defCmd.Bool("c", false, "Get number of bytes in file")
-	lineCount := defCmd.Bool("l", false, "Get number of lines in file")
-	wordCount := defCmd.Bool("w", false, "Get number of words in file")
-	charCount := defCmd.Bool("m", false, "Get number of characters in file")
-	byteLineWordCount := defCmd.Bool("", false, "Get number of bytes, lines, words in file")
-}
+	byteCount := flag.Bool("c", false, "Get number of bytes in file")
+	lineCount := flag.Bool("l", false, "Get number of lines in file")
+	wordCount := flag.Bool("w", false, "Get number of words in file")
+	charCount := flag.Bool("m", false, "Get number of characters in file")
+	byteLineWordCount := flag.Bool("", false, "Get number of bytes, lines, words in file")
+
+	if len(os.Args) < 1 {
+		fmt.Println("missing file name")
+		os.Exit(1)
+	}
+} 
