f4a2991ce4321efa38702c7041b24251fb45b1da
Satyaansh <satyanshasthana@gmail.com>
2023-10-06 18:20:28 +0530

    Add switch for input handling

--- main.go
+++ main.go
@@ -8,15 +8,23 @@ import (
 
 func main() {
 	
-	//inputs for defCmd
-	byteCount := flag.Bool("c", false, "Get number of bytes in file")
-	lineCount := flag.Bool("l", false, "Get number of lines in file")
-	wordCount := flag.Bool("w", false, "Get number of words in file")
-	charCount := flag.Bool("m", false, "Get number of characters in file")
-	byteLineWordCount := flag.Bool("", false, "Get number of bytes, lines, words in file")
+	defCmd := flag.NewFlagSet("ccwc", flag.ExitOnError)
+	byteCount := defCmd.Bool("c", false, "Get number of bytes in file")
+	lineCount := defCmd.Bool("l", false, "Get number of lines in file")
+	wordCount := defCmd.Bool("w", false, "Get number of words in file")
+	charCount := defCmd.Bool("m", false, "Get number of characters in file")
+	byteLineWordCount := defCmd.Bool("", false, "Get number of bytes, lines, words in file")
 
-	if len(os.Args) < 1 {
-		fmt.Println("missing file name")
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'ccwc' command")
 		os.Exit(1)
 	}
+
+	switch os.Args[1] {
+		case "ccwc": // if its the 'get' command
+			HandleCcwc()
+		default: // if we don't understand the input
+
+	}
+
 } 
